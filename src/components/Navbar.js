daa675df997138404254ae51024deb411ac782c2
Hariharasudhan <harim7434@gmail.com>
2026-09-08 23:11:35 +0530

    testing cd ci

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ export default function Navbar({ onOpenTerminal, currentTheme, setTheme, soundEn
     { name: "Projects", href: "#projects" },
     { name: "Experience", href: "#experience" },
     { name: "Contact", href: "#contact" },
-    { name: "demo", href: "#contact" },
+    { name: "demo----------", href: "#contact" },
   ];
 
   return (
